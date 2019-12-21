34aca15f07d723b39040016b2aa41a681ac0c07a
JeffD88 <42412051+JeffD88@users.noreply.github.com>
2019-12-20 20:00:17 -0500

    Add project files.

---
+++ FileLocker/Main.cs
@@ -0,0 +1,72 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Main.cs" company="CNC Software, Inc.">
+//   Copyright (c) 2019 CNC Software, Inc.
+// </copyright>
+// <summary>
+//  If this project is helpful please take a short survey at ->
+//  http://ux.mastercam.com/Surveys/APISDKSupport 
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace FileLocker
+{
+    using Mastercam.App;
+    using Mastercam.App.Types;
+
+    using FileLocker.Services;
+
+    public class Main : NetHook3App
+    {
+        #region Public Override Methods
+
+        /// <summary> The main entry point for your FileLocker. </summary>
+        ///
+        /// <param name="param"> System parameter. </param>
+        ///
+        /// <returns> A <c>MCamReturn</c> return type representing the outcome of your NetHook application. </returns>
+        public override MCamReturn Run(int param)
+        {
+            var fileLocker = new MastercamFileService();
+
+            if (fileLocker.IsFileOnDisk())
+            {
+                fileLocker.LockFile();
+            }
+            else
+            {
+                fileLocker.GenerateFileError();
+            }
+
+
+            return MCamReturn.NoErrors;
+        }
+
+        #endregion
+
+        #region Public User Defined Methods
+
+        /// <summary> The custom user function entry point for your FileLocker. </summary>
+        ///
+        /// <param name="param"> System parameter. </param>
+        ///
+        /// <returns> A <c>MCamReturn</c> return type representing the outcome of your NetHook application. </returns>
+        public MCamReturn RunUnlock(int param)
+        {
+            var fileLocker = new MastercamFileService();
+
+            if (fileLocker.IsFileOnDisk())
+            {
+                fileLocker.UnlockFile();
+            }
+            else
+            {
+                fileLocker.GenerateFileError();
+            }
+
+            return MCamReturn.NoErrors;
+        }
+
+        #endregion
+
+    }
+}
---
+++ FileLocker/Services/MastercamFileService.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+
+using Mastercam.IO;
+using Mastercam.IO.Types;
+
+namespace FileLocker.Services
+{
+    class MastercamFileService
+    {
+        public void LockFile()
+        {
+            File.SetAttributes(FileManager.CurrentFileName, FileAttributes.ReadOnly);
+
+            EventManager.LogEvent(MessageSeverityType.InformationalMessage,
+                                  FileManager.CurrentFileName,
+                                  "File Locked - Read-only attribute applied");
+
+            DialogManager.OK($"Read-only attribute applied to {Environment.NewLine}" +
+                             $"{FileManager.CurrentFileName}",
+                              "File Locked!");
+        }
+
+        public void UnlockFile()
+        {
+            File.SetAttributes(FileManager.CurrentFileName,
+                               File.GetAttributes(FileManager.CurrentFileName) & ~FileAttributes.ReadOnly);
+
+            EventManager.LogEvent(MessageSeverityType.InformationalMessage,
+                                 FileManager.CurrentFileName,
+                                 "File Unlocked - Read-only attribute removed");
+
+            DialogManager.OK($"Read-only attribute removed from {Environment.NewLine}" +
+                             $"{FileManager.CurrentFileName}",
+                              "File Unlocked!");
+        }
+
+        public bool IsFileOnDisk()
+        {
+            return File.Exists(FileManager.CurrentFileName);
+        }
+
+        public void GenerateFileError()
+        {
+            DialogManager.Error($"The current file doesn't exist {Environment.NewLine}" +
+                                $"{FileManager.CurrentFileName}",
+                                "File Not Saved!");
+        }
+
+    }
+}
